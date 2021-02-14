047f1909a2652dbaee15f74b5c6f2a9fc7503974
Maxwell-Hunt <max101hunt@gmail.com>
2021-02-14 09:17:51 -0500

    Commented server.py

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ from multiprocessing import Process, Manager
 import tkinter as tk
 import os
 
+#Important Constants
 HEADER = 64
 PORT = 5050
 IP = socket.gethostbyname(socket.gethostname())
@@ -17,14 +18,16 @@ LIGHT_THRESHOLD = 0.5
 SOUND_COMMAND_NOTIFY = "aplay ./Desktop/notify.wav"
 SOUND_COMMAND_REGISTER = "aplay ./Desktop/register.wav"
 
+# Function for sending messages from the sever to a client
 def send(client, message):
 	message_encoded = message.encode(FORMAT)
 	messsage_length = len(message_encoded)
 	send_length = str(messsage_length).encode(FORMAT)
 	send_length += b" " * (HEADER - len(send_length))
-	client.send(send_length)
-	client.send(message_encoded)
+	client.send(send_length)  # Send the length of the message
+	client.send(message_encoded)  # Send a message of that length
 
+# Function for listening to a client
 def handle_client(client, address, clients, global_namespace):
 	print(f"[NEW CONNECTION] {address} connected")
 
@@ -42,6 +45,7 @@ def handle_client(client, address, clients, global_namespace):
 		except ConnectionAbortedError:
 			break
 
+		# Check the client's message for specific requests
 		if msg == DISCONNECT_MESSAGE:
 			connected = False
 		elif msg == "!RESET":
@@ -63,6 +67,8 @@ def handle_client(client, address, clients, global_namespace):
 			del clients[i]
 			break
 		
+# Continuously broadcast the number of people in the store as well as the maximum capacity of the store
+# to all clients
 def broadcast(clients, global_namespace):
 	while True:
 		for client in clients:
@@ -72,14 +78,8 @@ def broadcast(clients, global_namespace):
 				pass
 		time.sleep(0.2)
 		
-# def gui(global_namespace):
-# 	root = tk.Tk()
-# 	def increment():
-# 		global_namespace.number_of_people += 1
-# 	btn1 = tk.Button(root, text = "Increment", command = increment)
-# 	btn1.pack()
-# 	root.mainloop()
-
+# Whenever a drop in light is detected by the raspberry pi
+# increment or decrement the number of people in the store and sound the speaker
 def check_ldr(global_namespace):
 	try:
 		from gpiozero import LightSensor
@@ -93,7 +93,7 @@ def check_ldr(global_namespace):
 		while True:
 			intersection = ldr.value < LIGHT_THRESHOLD
 
-			if intersection and not intersection_previous:
+			if intersection and not intersection_previous: # If the beam has just been broken
 				global_namespace.number_of_people = max(0, global_namespace.number_of_people + global_namespace.increment)
 				if global_namespace.number_of_people >= global_namespace.maximum_capacity and global_namespace.increment == 1:
 					os.system(SOUND_COMMAND_NOTIFY)
@@ -107,14 +107,15 @@ def check_ldr(global_namespace):
 
 if __name__ == "__main__":
 
+	# Set up the sockets
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.bind(ADDRESS)
 
 	with Manager() as manager:
-
 		clients = manager.list()
 		processes = []
 
+		# Create variables accessible from all threads
 		global_namespace = manager.Namespace()
 		global_namespace.number_of_people = 0
 		global_namespace.maximum_capacity = 100
@@ -122,17 +123,17 @@ if __name__ == "__main__":
 
 		print(f"[LISTENING] Server is listening on {IP}:{PORT}")
 		
+		# Start broadcast thread
 		broadcast_process = Process(target = broadcast, args = (clients, global_namespace))
 		broadcast_process.start()
 
+		# Start lightsensor checking thread
 		ldr_process = Process(target = check_ldr, args = (global_namespace,))
 		ldr_process.start()
 
-		# gui_process = Process(target = gui, args = (global_namespace,))
-		# gui_process.start()
-
+		
+		# Continuously listen for new clients and call handle_client for each one
 		server.listen()
-
 		while True:
 
 			client, address = server.accept()
